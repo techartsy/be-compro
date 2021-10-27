8d64f611d3cfa239b5b3d81c5010ac8f057eb0cd
Rinoto Harto <rinoto.harto@phincon.com>
2021-10-28 01:50:12 +0700

    fix defect

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ var path = require("path");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-// app.use("/uploads", express.static("uploads"));
 // app.use(express.static("../uploads"));
-console.log(__dirname, "<< dirname");
-app.use(express.static(path.join(__dirname, "/public")));
+// console.log(__dirname, "<< dirname");
+// app.use(express.static(path.join(__dirname, "/public")));
 // process.env.PWD = process.cwd();
 // app.use(express.static(path.join(process.env.PWD, "public")));
 // app.use('/static', express.static(__dirname + '/public'));
 // app.use(express.static(__dirname + "../public"));
 app.use("/app/v1/techartsy", router);
+app.use("/public", express.static("public"));
 app.get("/", (err, res) => {
   res.send("Server Run");
 });

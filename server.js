6f4db30dd8b26b1cec39c2ac95bdafb522ad7623
Rinoto Harto <rinoto.harto@phincon.com>
2021-10-28 01:13:45 +0700

    fix defect

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/uploads", express.static("uploads"));
+// app.use("/uploads", express.static("uploads"));
+app.use(express.static(path.join(__dirname, "./uploads")));
 app.use("/app/v1/techartsy", router);
 app.get("/", (err, res) => {
   res.send("Server Run");
